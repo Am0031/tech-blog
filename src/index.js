3182c1b3f672fccc45e70e3806ba3cd83dbda90d
Amelie Pira <amelie.pira@gmail.com>
2022-07-06 22:01:45 +0100

    set up main index.js

---
+++ src/index.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const express = require("express");
+const exphbs = require("express-handlebars");
+
+const connection = require("./config/connection");
+const routes = require("./routes");
+const helpers = require("./helpers");
+
+const PORT = process.env.PORT || 4000;
+
+const hbs = exphbs.create({ helpers });
+
+const app = express();
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "../public")));
+app.use(routes);
+
+const init = async () => {
+  try {
+    // connect to DB
+    await connection.sync({ force: false });
+
+    // server listen on PORT
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.log(`[ERROR]: Failed to start server | ${error.message}`);
+  }
+};
+
+init();
