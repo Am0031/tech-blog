cd125fe35dd92013b601dc2b0f379b5624a36342
Amelie Pira <amelie.pira@gmail.com>
2022-07-07 16:15:24 +0100

    added and tested create comment function

--- src/controllers/api/comments.js
+++ src/controllers/api/comments.js
@@ -1,6 +1,24 @@
-const createComment = (req, res) => {
-  console.log("create comment");
-  return res.json({ message: "create comment" });
+const { User, Post, Comment } = require("../../models");
+
+const createComment = async (req, res) => {
+  try {
+    const { postId, commentText } = req.body;
+    if (!commentText) {
+      return res.status(400).json({ message: "Unable to create comment" });
+    }
+
+    // FOR NOW: hard code user id
+    // Later, get user id from logged in session object
+    const userId = 1;
+
+    const newComment = await Comment.create({ commentText, userId, postId });
+    return res
+      .status(200)
+      .json({ message: "Comment created", newComment: newComment });
+  } catch (error) {
+    console.error(`ERROR | ${error.message}`);
+    return res.status(500).json(error);
+  }
 };
 const deleteComment = (req, res) => {
   console.log("delete comment by id");
