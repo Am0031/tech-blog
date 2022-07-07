a87dc999135f3e80a58e2a2eeca7b26c8281c938
Amelie Pira <amelie.pira@gmail.com>
2022-07-07 13:49:50 +0100

    tested routes in postman (auth disabled for now)

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,10 +1,12 @@
 const auth = (req, res, next) => {
-  if (req.session.isLoggedIn) {
-    console.log(`[INFO]: User ${req.session.user.email} is in session`);
-    next();
-  } else {
-    res.redirect("/login");
-  }
+  // if (req.session.isLoggedIn) {
+  //   console.log(`[INFO]: User ${req.session.user.email} is in session`);
+  //   next();
+  // } else {
+  //   res.redirect("/login");
+  // }
+  console.log(`[INFO]: User is in session`);
+  next();
 };
 
 module.exports = auth;
