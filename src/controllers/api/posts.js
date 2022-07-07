9e9f083b85f5afcc6ba485d541d57ab53c40bab2
Amelie Pira <amelie.pira@gmail.com>
2022-07-07 14:12:19 +0100

    added get posts functions

--- src/controllers/api/posts.js
+++ src/controllers/api/posts.js
@@ -1,10 +1,33 @@
-const getAllPosts = (req, res) => {
-  console.log("get all posts");
-  return res.json({ message: "get all posts" });
+const { User, Post, Comment } = require("../../models");
+
+const getAllPosts = async (req, res) => {
+  try {
+    const posts = await Post.findAll({
+      include: [{ model: User, attributes: ["userName"] }],
+    });
+    if (!posts) {
+      return res.status(500).json({ message: "Posts not found" });
+    }
+    return res.json(posts);
+  } catch (error) {
+    console.error(`ERROR | ${error.message}`);
+    return res.status(500).json(error);
+  }
 };
-const getPostById = (req, res) => {
-  console.log("get post by id");
-  return res.json({ message: "get post by id" });
+
+const getPostById = async (req, res) => {
+  try {
+    const post = await Post.findAll({
+      include: [{ model: Comment }, { model: User }],
+    });
+    if (!post) {
+      return res.status(500).json({ message: "Posts not found" });
+    }
+    return res.json(post);
+  } catch (error) {
+    console.error(`ERROR | ${error.message}`);
+    return res.status(500).json(error);
+  }
 };
 
 const createPost = (req, res) => {
