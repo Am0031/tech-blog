ff7b7a5fe7796e8bcbea8955305fd98e66e418ca
Amelie Pira <amelie.pira@gmail.com>
2022-07-10 17:08:13 +0100

    added handleSignup function (wip)

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,6 +1,38 @@
-const handleSignup = (event) => {
+const setPasswordConfirmValidity = () => {};
+
+const handleSignup = async (event) => {
   event.preventDefault();
-  console.log("handling signup");
+
+  const username = $("#inputUsername").val().trim();
+  const email = $("#inputEmail").val().trim();
+  const password = $("#inputPassword1").val();
+  const confirmPassword = $("#inputPassword2").val();
+
+  if (password === confirmPassword) {
+    $("#inputPassword2").setCustomValidity("");
+  } else {
+    $("#inputPassword2").setCustomValidity("Passwords must match.");
+    return false;
+  }
+
+  const userInfo = { username, email, password };
+
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    redirect: "follow",
+    body: JSON.stringify(userInfo),
+  };
+
+  const response = await fetch("/auth/signup", options);
+
+  if (response.status !== 200) {
+    console.error("Signup failed");
+  } else {
+    window.location.replace("/dashboard");
+  }
 };
 
 const handleLogin = (event) => {
