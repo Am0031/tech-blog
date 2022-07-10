209272587fe0d7894d85c3ebc0045bbb76ce5f1d
Amelie Pira <amelie.pira@gmail.com>
2022-07-10 18:15:37 +0100

    added render dashboard view function

--- src/controllers/views/privateViews.js
+++ src/controllers/views/privateViews.js
@@ -1,14 +1,36 @@
 const path = require("path");
+const dataProvider = require("../../dataProvider");
 
-const renderDashboardPage = (req, res) => {
-  const filePath = path.join(__dirname, "../../../public/dashboard.html");
-  return res.sendFile(filePath);
+const renderDashboardPage = async (req, res) => {
+  try {
+    const posts = await dataProvider.getAllPosts();
+    const { isLoggedIn } = req.session;
+    const { id, username } = req.session.user;
+
+    const userPosts = posts.map((d) => d.dataValues).filter((i) => i.id === id);
+
+    const formatData = (each) => {
+      const id = each.id;
+      const title = each.title;
+      const date = each.createdAt;
+      return {
+        id,
+        title,
+        date,
+      };
+    };
+    const viewModel = userPosts.map(formatData);
+    return res.render("dashboard", { isLoggedIn, username, data: viewModel });
+  } catch (error) {
+    console.log(`${error.message}`);
+    res.render("error");
+  }
 };
-const renderEditPost = (req, res) => {
+const renderEditPost = async (req, res) => {
   const filePath = path.join(__dirname, "../../../public/editPost.html");
   return res.sendFile(filePath);
 };
-const renderCreatePost = (req, res) => {
+const renderCreatePost = async (req, res) => {
   const filePath = path.join(__dirname, "../../../public/createPost.html");
   return res.sendFile(filePath);
 };
