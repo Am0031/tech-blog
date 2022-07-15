aca4c13c8b8aa3d55eadaa54167b5a6413570f96
Amelie Pira <amelie.pira@gmail.com>
2022-07-15 11:03:25 +0100

    added formatDate helper for shorter date display

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,7 +1,14 @@
+const moment = require("moment");
+
+const formatDate = (date) => {
+  return moment(date).format("DD/MM/YYYY - HH:mm");
+};
+
 const isEqualTo = (data, equalToString) => {
   return data === equalToString;
 };
 
 module.exports = {
   isEqualTo,
+  formatDate,
 };
